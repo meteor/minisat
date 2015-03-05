26e00d4d75b6dc81b00cd0acbeb6a0765aecb5bd
David Greenspan <dgreenspan@alum.mit.edu>
2015-03-05 08:22:28 -0800

    Use a dummy "0" variable instead of saying v-1
    As discussed in code review, remembering to shift
    the variable number by 1 in all the right places
    is a source of bugs and makes the code harder to
    understand and debug.

--- meteor/logic-solver.cc
+++ meteor/logic-solver.cc
@@ -18,19 +18,29 @@ extern "C" {
     delete s;
   }
 
-  // One global solver.  We instantiate the entire C environment
-  // per JavaScript solver object.
-  SimpSolver *SOLVER;
+  // The number of variables allocated, not including the
+  // dummy "0" variable we give to MiniSat.
   int NumVars = 0;
+
+  // One global solver.  We instantiate the entire C environment
+  // per JavaScript solver object.  Only call createTheSolver()
+  // once (per C environment).
+  SimpSolver *SOLVER;
   void createTheSolver() {
     SOLVER = new SimpSolver();
     // Turn off variable elimination, which is problematic for
     // incremental solving.
     SOLVER->eliminate(/*turn_off_elim = */true);
+    SOLVER->newVar(); // create dummy "0" variable
   }
 
-  // v is 1-based
+  // Make sure enough variables have been allocated to hold
+  // "v".
   void ensureVar(int v) {
+    // For example, suppose v is 4 and NumVars is 3.
+    // That means MiniSat has allocated space for vars
+    // 0, 1, 2, and 3 (where "0" is a dummy var).
+    // The loop body is run once to allocate room for var 4.
     while (v > NumVars) {
       SOLVER->newVar();
       NumVars++;
@@ -38,8 +48,8 @@ extern "C" {
   }
   
   // terms is 0-terminated.  Negative numbers are negative
-  // terms.  1, 2, and 3 mean variables 0, 1, and 2.
-  // -1, -2, and -3 mean "not 0", "not 1", and "not 2".
+  // terms.  1, 2, and 3 mean variables 1, 2, and 3.
+  // -1, -2, and -3 mean "not 1", "not 2", and "not 3".
   bool addClause(int *terms) {
     vec<Lit> vars;
     while (*terms) {
@@ -50,7 +60,7 @@ extern "C" {
         isNot = true;
       }
       ensureVar(v);
-      vars.push(mkLit(Var(v-1), isNot));
+      vars.push(mkLit(Var(v), isNot));
       terms++;
     }
     return SOLVER->addClause(vars);
@@ -60,6 +70,11 @@ extern "C" {
     return SOLVER->solve();
   }
 
+  // Returns a pointer to a sequence of bytes representing
+  // the solution values of the variables, starting with
+  // variable 1 (that is, the first byte is variable 1,
+  // the second byte is variable 2, etc.).  A value of
+  // 0 means "true", and anything else means "false".
   uint8_t * getSolution() {
     // model is a vec<lbool>, which has a cast operator
     // that gives you a lbool* (a pointer to the data
@@ -67,7 +82,13 @@ extern "C" {
     // true, false, or undefined (0, 1, or 2).  It's a
     // class containing a uint8 field, but in memory it
     // just looks like a uint8.
-    return (uint8_t *)((lbool *)SOLVER->model);
+    //
+    // MiniSat does not distinguish between false and
+    // undefined in solutions (I think it sets undefined
+    // variables to false).
+    //
+    // Adding 1 to the pointer skips the dummy "0" variable.
+    return (uint8_t *)((lbool *)SOLVER->model) + 1;
   }
 
   int getNumVars() {
@@ -75,31 +96,34 @@ extern "C" {
   }
 
   // solve while temporarily assuming variable v is true.
-  // v is 1-based.
   bool solveAssuming(int v) {
     vec<Lit> assumps;
-    assumps.push(mkLit(Var(v-1)));
+    assumps.push(mkLit(Var(v)));
     return SOLVER->solve(assumps,
                          /*do_sump = */true,
                          /*turn_off_simp = */false);
   }
   
   // mark a Var as false, forever.
-  // v is 1-based.
   void retireVar(int v) {
-    SOLVER->addClause(mkLit(Var(v-1), true));
+    SOLVER->addClause(mkLit(Var(v), true));
   }
 
   int getConflictClauseSize() {
     return SOLVER->conflict.size();
   }
 
+  // The "conflict clause" feature of MiniSat is not what it sounds
+  // like, unfortunately -- it doesn't help explain conflicts.
+  // It only tells us which assumption vars are to blame for a failed
+  // solveAssuming (and we only ever pass one var).
+  // We keep this function around in case we discover a use for it.
+  //
   // Returns a pointer to an int[] of length getConflictClauseSize().
   int * getConflictClause() {
     // conflict is an IntSet<Lit>, which we can cast
     // to an array of int, where each int is 2*v + s where
-    // v is the zero-based variable number (the 1-based variable number
-    // minus one) and s is 1 for "not".
+    // v is the variable number and s is 1 for "not".
     return (int *)((Lit *)*const_cast<vec<Lit> *>(&SOLVER->conflict.toVec()));
   }
 };
